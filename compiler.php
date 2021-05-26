2bd3f79333b3e7e3dcc9278cb81bdbd291bab7d8
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-25 21:28:13 -0400

    Update compiler.php

--- compiler.php
+++ compiler.php
@@ -1,28 +1,8 @@
 <?php 
 namespace Doge;
 
-/**
- * Compiler interface
- * 
- * @package phpdoge
- */
- 
-interface Compiler {
-    
-    /**
-     * @param array $grammar
-     */
-    public function __construct ($grammar);
-    
-    /**
-     * @param array $tokens
-     * @return string
-     */
-    public function compile ($tokens);
-    $tokens = isset($_POST['tokens']);
-}
-
-
-if(!$tokens){
-  echo"Foobar";
+if(isset($_POST['tokens'])){
+ $tokens = htmlentities($_POST['tokens']);
+}else{
+ exit();
 }
