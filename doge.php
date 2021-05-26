65b6fa0a1378643dece675cd348407af3f2aebe6
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-25 22:30:19 -0400

    Added basic web interpreter
    Highly experimental, this update allows the rewriting of the preset syntax in the form of a GET request

--- doge.php
+++ doge.php
@@ -1,10 +1,35 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT']."/compiler.php");
 
-class doge{
+if(isset($_GET['c'])){
+    if(!isset($command)){
+        $command = htmlspecialchars($_GET['c']); //dogeSay'hi';
+    }
+}else{
+    // no command found instantiating default method
+        //$command = "dogeSay'test'.";
+}
+
+if(!isset($command)){ // use outside else in case of get request not running
+    $command = "dogeSay'test'.";
+}
+
+class deobfuscate{
+  public function rewrite(){
+      global $command;
+      //      example - $command = "dogeSay'test'.";
+
+    $replacerFunc = str_replace("dogeSay", "echo", "".$command."");
+    $replacerPeriod = str_replace(".", ";", "".$replacerFunc."");
+        echo $replacerPeriod;
+  }
+  
+}
+
+class doge extends deobfuscate{
 
 public function interpreter(){
-  echo "PHPDoge Library by DreamingCoders";
+  echo "<script>console.log('PHPDoge Library by DreamingCoders');</script>";
 }
 
 public function sha512(){
@@ -19,14 +44,28 @@ private function wow(){
 */
 
 private function sha512Verify(){
- // Solve/verify hash (Award)
-  //$foo->bar(award());
-    //$foo->wow();
-}
-  
+ /* Solve/verify hash (Award)
+ * Usage
+  * $foo->bar(award());
+    * $foo->wow();
+  */
 }
 
+}
+
+if(!isset($command)){
+    $command = "dogeSay\"Hello\"\;";
+}
+
+$deobfuscate = new deobfuscate;
 $doge = new doge;
-echo $doge->interpreter();
-echo $doge->sha512();
+
+$rewriteALG = $deobfuscate->rewrite();
+
+  // Load core doge
+    echo $doge->interpreter()."<br>";
+    //echo $doge->sha512();
+
+        echo $rewriteALG;
+
 ?>
