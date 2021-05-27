07ede3fea87b851193d579cdd4c5c9074139972f
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-26 23:52:02 -0400

    Update shibesql.php

--- shibesql.php
+++ shibesql.php
@@ -4,27 +4,28 @@
 $globalConnection = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'password');
 
 //pdo oo connection to database
-class sqlmain{
-
+class shibesql{
+  pubic function shibeSql(){ //experimental approach. those who fork please look into the security aspects of this function
+    return $globalConnection;
 }
 
-class shibesql extends sqlmain{
-pubic function shibeSql(){ //experimental approach. those who fork please look into the security aspects of this function
-return $globalConnection;
-}
-//$globalConnection
+class sqlmain extends shibesql{
+    public function protect(){
+      return bindParam(); 
+    }
+  }
+  
+  //$globalConnection->exec('INSERT INTO `very` VALUES (1, "somevalue"');
 
-//$globalConnection->exec('INSERT INTO `very` VALUES (1, "somevalue"');
 
-
-}
+  }
 
 }
 
 $walletId = md5(uniqid("param"));
 
 shibeSql()->prepare("SELECT * FROM `wallets` WHERE `id` = :id");
-shibeSql()->bindParam(":id", $walletId, PDO::PARAM_INT);
+shibeSql()->protect(":id", $walletId, PDO::PARAM_INT);
   shibeSql()->execute();
 
 ?>
