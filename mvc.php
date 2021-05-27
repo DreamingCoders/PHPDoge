df9a788455ce0c43ad7cc8b7272737fa699020f1
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 19:26:24 -0400

    Update mvc.php
    Made it check if MVC is enabled from install otherwise do not use MVC based on user-choice

--- mvc.php
+++ mvc.php
@@ -2,14 +2,12 @@
 // DreamingCoders
 include($_SERVER['DOCUMENT_ROOT']."/core/config.php");
 
-if(isset($mvc)){ // check if mvc is instantiated
-
-  // note: this is not the correct mvc variable I am concept-writing this
-  
-  
-  define('Public', str_replace("Public/index.php", "", "".$_SERVER['SCRIPT_NAME']."");
-  require($_SERVER['DOCUMENT_ROOT']."/router.php");
-  require($_SERVER['DOCUMENT_ROOT']."/request.php");
+if(isset($mvcEnabled)){ // check if mvc is instantiated  
+  if($mvcEnabled == "true"){
+    define('Public', str_replace("Public/index.php", "", "".$_SERVER['SCRIPT_NAME']."");
+    require($_SERVER['DOCUMENT_ROOT']."/router.php");
+    require($_SERVER['DOCUMENT_ROOT']."/request.php");
+  }
   
 }else{
   exit(); 
