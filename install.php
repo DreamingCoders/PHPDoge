c8e48203bd67910cb4ccd7b29c53c48f28dc07a3
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 18:40:41 -0400

    Update install.php

--- install.php
+++ install.php
@@ -1,11 +1,14 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/config.php");
+$actual_link = "http://".$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]."";
 ?>
 
 <script>
   installDoge = function installDoge();
   function installDoge(){
     alert("Loading installation.");
+    alert("Installation complete. Please do not delete this file as it is still required for PHPDoge to run.\
+          View your application live at: <?=$actual_link?>");
   }
 </script>
 
@@ -16,6 +19,8 @@ include($_SERVER['DOCUMENT_ROOT']."/config.php");
       <h2>PHPDoge Install</h2>
         <p>Automatically configure your website by installing PHPDoge</p>
           <p>If you use install.php to install PHPDoge, config.php will be manually initialized.</p>
+      <div class="card-divider"></div>
+      
       <form action="#" method="GET">
           <input type="text" name="serverName" class="form-control">
             <input id="installDoge" type="submit" name="submitInstall" class="btn btn-success" value="Install">
