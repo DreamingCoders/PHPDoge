2ff39da191e06b70c8018aa7401c50d14f328dc2
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 19:22:07 -0400

    Update mvc.php

--- mvc.php
+++ mvc.php
@@ -5,6 +5,12 @@ include($_SERVER['DOCUMENT_ROOT']."/core/config.php");
 if(isset($mvc)){ // check if mvc is instantiated
 
   // note: this is not the correct mvc variable I am concept-writing this
+  
+  
+  define('Public', str_replace("Public/index.php", "", "".$_SERVER['SCRIPT_NAME']."");
+  require($_SERVER['DOCUMENT_ROOT']."/router.php");
+  require($_SERVER['DOCUMENT_ROOT']."/request.php");
+  
 }else{
   exit(); 
 }
