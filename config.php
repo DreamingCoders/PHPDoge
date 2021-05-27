78768db85b9949e671d4afd479f74735d378a384
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 18:43:28 -0400

    Update config.php (Installation 25%)
    Allowed config.php to manually configure with install.php
    est. 25%

--- config.php
+++ config.php
@@ -3,6 +3,15 @@
   * it is recommended to add on to config.php with your own configurations as you progress in your application
 */
 
+if(isset($_GET['installSuccess'])){
+
+  $installSuccess = "true";
+   if($installSuccess == "true"){
+      // Overwrite core variables for simplicity
+   }
+ 
+}
+
 $allowSourceProtection = "false"; // Set to true if you would like to configure an automated process to protect your PHP code
 
 if(isset($allowSourceProtection){
