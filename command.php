3a7693744a2d8c151fa70e0d90b8eaf91f1493ab
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 00:24:45 -0400

    Create command.php
    Goes along with doge.php

---
+++ command.php
@@ -0,0 +1,8 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT']."doge.php");
+?>
+
+<form action="#" method="GET">
+  <input type="text" class="form-control" name="c" placeholder="Enter Dogecode to compile to PHP">
+  <input type="submit" class="btn btn-success" name="submitCommand" value="Submit">
+</form>
