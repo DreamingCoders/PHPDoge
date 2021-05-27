ec351c2065a4de5d144eb0e154f350301f739bcb
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 18:57:43 -0400

    Create mvc.php

---
+++ mvc.php
@@ -0,0 +1,10 @@
+<?php
+// DreamingCoders
+include($_SERVER['DOCUMENT_ROOT']."/core/config.php");
+
+if(isset($mvc)){ // check if mvc is instantiated
+
+  // note: this is not the correct mvc variable I am concept-writing this
+}else{
+  exit(); 
+}
