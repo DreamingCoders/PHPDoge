d10ba03245fa389bfe1a9cf1831b7a973e9a0f6c
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 22:49:10 -0400

    Update transport.php

--- transport.php
+++ transport.php
@@ -1,3 +1,9 @@
 <?php
-$transport = "noMVC"; // Set to MVC or noMVC depending on how you want your framework to run.
+//transport.php
+// DreamingCoders
+$mvcEnabled = "false";
+
+
+$transport = "noMVC"; // Set to MVC or noMVC depending on how you want your framework to run. This variable is unused at the moment
+
 ?>
