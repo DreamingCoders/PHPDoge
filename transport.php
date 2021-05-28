59ea010576941e83c38c228551eb0d1dc4ee209c
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 22:51:21 -0400

    Update transport.php

--- transport.php
+++ transport.php
@@ -1,9 +1,10 @@
 <?php
-//transport.php
+//transport.php *Handles major aspects of the PHPDoge framework based on users choice*
 // DreamingCoders
 $mvcEnabled = "false";
 
 
 $transport = "noMVC"; // Set to MVC or noMVC depending on how you want your framework to run. This variable is unused at the moment
 
+$transportTypes = array("Forum", "Learning", "Blog", "Social Network");
 ?>
