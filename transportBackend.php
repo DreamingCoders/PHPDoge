c89f75665cb96f5418a92c4b06158be10385cc5d
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 22:59:00 -0400

    Create transportBackend.php

---
+++ transportBackend.php
@@ -0,0 +1,48 @@
+<div class="container">
+<?php
+// Forum
+?>
+  <div class="card">
+    <div class="card-header">Forum</div>
+    <div class="card-body">Forum description</div>
+  </div>
+<?php
+// Blog
+?>
+    <div class="card">
+    <div class="card-header">Blog</div>
+    <div class="card-body">Blog Post</div>
+  </div>
+<?php
+// Learn
+?>
+<?php
+// Auth System
+?>
+  <div class="row">
+    <div class="col-md-6">
+        <div class="card">
+          <div class="card-header bg-success text-white">Login</div>
+          <div class="card-body">
+            <form action="#" method="POST">
+              <input type="text" name="username" class="form-control" placeholder="Username">
+            </form>
+          </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+          <div class="card-header bg-primary text-white">Register</div>
+          <div class="card-body">
+            <form action="#" method="POST">
+              <input type="text" name="username" class="form-control" placeholder="Username">
+            </form>
+          </div>
+        </div>
+    </div>
+  </div>
+<?php
+  // End transport backend
+// DreamingCoders
+?>
+</div>
