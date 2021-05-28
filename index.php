68f02e6ccad4a508ac17ca1dd9b6c69eb69b5627
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 22:19:44 -0400

    Update index.php
    Made it check if installation was handled otherwise continue with framework

--- index.php
+++ index.php
@@ -4,6 +4,29 @@
   // First page you will see on your server
   
 include($_SERVER['DOCUMENT_ROOT']."/config.php");
+$rerouter = "<script>window.location.replace('/install.php');</script>".
+      exit();
+/* GET request already handled on config, added for reference purposes
+if(isset($_GET['installSuccess'])){
+
+  $installSuccess = "true";
+   if($installSuccess == "true"){
+      // Overwrite core variables for simplicity
+   }
+
+}
+*/
+
+if(isset($installSuccess)){
+  if($installSuccess == "true"){
+    // Main website page handler
+  }else{
+    return $rerouter;
+  }
+}else{
+  return $rerouter;
+}
+
 ?>
 
 <div class="container">
@@ -12,4 +35,6 @@ include($_SERVER['DOCUMENT_ROOT']."/config.php");
 
 <?php
 //include($_SERVER['DOCUMENT_ROOT']."/footer.php);
+// Note: check if rerouter variable compiles
+  //DreamingCoders 2021
 ?>
