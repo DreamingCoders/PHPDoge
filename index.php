81776eff9afa5bc2697cee55dba50a45f9ceb9d7
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-05-27 22:14:08 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// index.php
+// DreamingCoders
+  // First page you will see on your server
+  
+include($_SERVER['DOCUMENT_ROOT']."/config.php");
+?>
+
+<div class="container">
+
+</div>
+
+<?php
+//include($_SERVER['DOCUMENT_ROOT']."/footer.php);
+?>
