2ed8e3af2d33b180b65586ff7a9ca0d88a71f8d4
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-06-06 01:43:24 -0400

    Added first example route

--- router.php
+++ router.php
@@ -2,5 +2,19 @@
 //DreamingCoders
 require $_SERVER['DOCUMENT_ROOT'].'/core/router-steptwo.php';
 
-// Place your routes here
-    // for usage read documentation or steptwo.php if you know what you are doing.
+    // For usage read documentation or steptwo.php
+        // begin routes
+            ROUTER::get('/index.php', function($req,$res,$next){
+
+                // Do something here
+                echo "Route system works!" ;
+
+                // call Next Callback, control goes to next callback function
+                $next();
+
+            },function($req,$res){
+
+
+                echo "END";  // task completed
+
+            });
