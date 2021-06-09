d3ed4f6b34b2b2d7c1a8eed51259b5c44e14e5da
Colin <61954544+DreamingCoders@users.noreply.github.com>
2021-06-08 22:54:32 -0400

    Create captcha.php (hcaptcha)

---
+++ html/captcha.php
@@ -0,0 +1,2 @@
+<script src="https://hcaptcha.com/1/api.js" async defer></script>
+<div id="hcaptcha" class="h-captcha" data-sitekey="8492a93b-fc96-487d-a2e1-f05b9c4c754f" data-callback="onSuccess" data-expired-callback="onExpire"></div>
